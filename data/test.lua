268b29d3754a9f5c571d175b0a14b79f9591fe71
manuelwestermeier <westermeier111@gmail.com>
2025-07-21 18:49:09 +0200

    formatted lua file

--- data/test.lua
+++ data/test.lua
@@ -1,36 +1,57 @@
 -- Morse map
 morse = {
-  ["A"] = ".-", ["B"] = "-...", ["C"] = "-.-.", ["D"] = "-..", ["E"] = ".", ["F"] = "..-.",
-  ["G"] = "--.", ["H"] = "....", ["I"] = "..", ["J"] = ".---", ["K"] = "-.-", ["L"] = ".-..",
-  ["M"] = "--", ["N"] = "-.", ["O"] = "---", ["P"] = ".--.", ["Q"] = "--.-", ["R"] = ".-.",
-  ["S"] = "...", ["T"] = "-", ["U"] = "..-", ["V"] = "...-", ["W"] = ".--", ["X"] = "-..-",
-  ["Y"] = "-.--", ["Z"] = "--.."
+    ["A"] = ".-",
+    ["B"] = "-...",
+    ["C"] = "-.-.",
+    ["D"] = "-..",
+    ["E"] = ".",
+    ["F"] = "..-.",
+    ["G"] = "--.",
+    ["H"] = "....",
+    ["I"] = "..",
+    ["J"] = ".---",
+    ["K"] = "-.-",
+    ["L"] = ".-..",
+    ["M"] = "--",
+    ["N"] = "-.",
+    ["O"] = "---",
+    ["P"] = ".--.",
+    ["Q"] = "--.-",
+    ["R"] = ".-.",
+    ["S"] = "...",
+    ["T"] = "-",
+    ["U"] = "..-",
+    ["V"] = "...-",
+    ["W"] = ".--",
+    ["X"] = "-..-",
+    ["Y"] = "-.--",
+    ["Z"] = "--.."
 }
 
 function flash(symbol)
-  if symbol == "." then
-    setLED(1)
-    delay(200)
-    setLED(0)
-  elseif symbol == "-" then
-    setLED(1)
-    delay(600)
-    setLED(0)
-  end
-  delay(200) -- pause between symbols
+    if symbol == "." then
+        setLED(1)
+        delay(200)
+        setLED(0)
+    elseif symbol == "-" then
+        setLED(1)
+        delay(600)
+        setLED(0)
+    end
+    delay(200) -- pause between symbols
 end
 
 msg = "HELLO"
 print("Morse: " .. msg)
 
 for c in msg:gmatch(".") do
-  c = c:upper()
-  code = morse[c]
-  if code then
-    print(c .. ": " .. code)
-    for i = 1, #code do
-      flash(code:sub(i,i))
+    c = c:upper()
+    code = morse[c]
+    if code then
+        print(c .. ": " .. code)
+        for i = 1, #code do
+            flash(code:sub(i, i))
+        end
+        delay(600) -- pause between letters
     end
-    delay(600) -- pause between letters
-  end
 end
