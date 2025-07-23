c51ee457292fe922d7f47886025dfae492db08b3
ManuelWestermeier <westermeier111@gmail.com>
2025-07-23 23:53:37 +0200

    .

--- src/screen/config.h
+++ src/screen/config.h
@@ -77,12 +77,12 @@
 // #define TFT_WIDTH  80
 // #define TFT_WIDTH  128
 // #define TFT_WIDTH  128 // ST7789 240 x 240 and 240 x 320
-#define TFT_WIDTH 320
+#define TFT_WIDTH 240
 // #define TFT_WIDTH  320
 // #define TFT_HEIGHT 160
 // #define TFT_HEIGHT 128
 // #define TFT_HEIGHT 160 // ST7789 240 x 240
-#define TFT_HEIGHT 240 // ST7789 240 x 320
+#define TFT_HEIGHT 320 // ST7789 240 x 320
 // #define TFT_HEIGHT 240 // GC9A01 240 x 240 //#define TFT_HEIGHT 480
 // #define TFT_HEIGHT 480 //
 
