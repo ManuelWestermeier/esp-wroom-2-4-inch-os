30427f455f8061b9eab3bec1da14d126f9a8a7de
ManuelWestermeier <westermeier111@gmail.com>
2025-07-23 22:53:21 +0200

    .

--- src/main.cpp
+++ src/main.cpp
@@ -26,10 +26,14 @@
 // }
 #include "./screen/config.h"
 
-#include <TFT_eSPI.h> // Bibliothek von Bodmer
+#include <TFT_eSPI.h> // Bodmer's TFT library
 #include <SPI.h>
 
-TFT_eSPI tft = TFT_eSPI(); // TFT instance
+TFT_eSPI tft = TFT_eSPI();   // TFT instance
+TFT_eSPI_Button touchButton; // Optional: Für Buttons
+
+uint16_t touchX = 0, touchY = 0; // Touch-Koordinaten
+bool touched = false;
 
 void setup()
 {
@@ -38,6 +42,7 @@ void setup()
 
     delay(5000);
     Serial.begin(115200);
+
     tft.init();
     tft.setRotation(1); // Querformat
     tft.fillScreen(TFT_BLACK);
@@ -46,9 +51,26 @@ void setup()
     tft.setTextSize(2);
     tft.setCursor(20, 100);
     tft.println("Hello, World!!");
+
+// Touch kalibrieren, wenn nötig
+#ifdef TOUCH_CS
+    tft.begin();
+#endif
+}
+
+// Funktion zum Zeichnen einer Box an der Touch-Stelle
+void drawBoxAt(uint16_t x, uint16_t y)
+{
+    const int boxSize = 20;
+    tft.drawRect(x - boxSize / 2, y - boxSize / 2, boxSize, boxSize, TFT_RED);
 }
 
 void loop()
 {
-    // nichts
+    if (tft.getTouch(&touchX, &touchY))
+    {
+        Serial.printf("Touch at: %d, %d\n", touchX, touchY);
+        drawBoxAt(touchX, touchY);
+        delay(300); // Entprellung
+    }
 }
