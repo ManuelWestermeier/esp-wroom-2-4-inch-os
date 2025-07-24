23ca6973e6ea812d1dea16b925e07e118504f8ac
manuelwestermeier <westermeier111@gmail.com>
2025-07-24 22:02:51 +0200

    working

--- src/apps/window.hpp
+++ src/apps/window.hpp
@@ -8,85 +8,140 @@ struct Window
 {
     TFT_eSprite vScreen = TFT_eSprite(&Screen::tft);
 
-    Vec off;       // Fensterposition
-    Rect dragArea; // Bereich zum Ziehen
-    Rect closeBtn; // Bereich für Schließen-Button
-    String name;   // Fenstertitel
+    Vec off;              // Fensterposition
+    Vec size = {160, 90}; // Fenstergröße
+    Rect dragArea;
+    Rect closeBtn;
+    Rect resizeArea;
+    String name;
 
-    static constexpr Vec windowSize = {160, 90};
+    bool isDragging = false;
+    bool isResizing = false;
+
+    Vec lastTouchPos;
+
+    static constexpr Vec minSize = {80, 60};
+    static constexpr Vec maxSize = {240, 160};
     static constexpr int titleBarHeight = 12;
     static constexpr int closeBtnSize = 12;
+    static constexpr int resizeBoxSize = 12;
 
     void init(String windowName, Vec margin = {50, 50})
     {
         off = margin;
         name = windowName;
 
-        vScreen.createSprite(windowSize.x, windowSize.y); // ~20 KB RAM
+        vScreen.createSprite(size.x, size.y);
         vScreen.setTextColor(TFT_BLACK);
     }
 
     void drawTitleBar()
     {
-        Rect frame = {off - Vec{1, titleBarHeight + 1},
-                      windowSize + Vec{2, titleBarHeight + 2}};
+        Rect frame = {off - Vec{1, titleBarHeight + 1}, size + Vec{2, titleBarHeight + 2}};
         Screen::tft.drawRect(frame.pos.x, frame.pos.y, frame.dimensions.x, frame.dimensions.y, TFT_BLACK);
 
-        // Drag-Area: alles außer Close-Button
-        dragArea = {off - Vec{0, titleBarHeight}, {windowSize.x, titleBarHeight}};
+        dragArea = {off - Vec{0, titleBarHeight}, {size.x, titleBarHeight}};
         Rect dragFill = dragArea;
         dragFill.dimensions.x -= closeBtnSize;
         Screen::tft.fillRect(dragFill.pos.x, dragFill.pos.y, dragFill.dimensions.x, dragFill.dimensions.y, RGB(220, 220, 250));
 
-        // Fenstertitel
         Screen::tft.setTextSize(1);
         Screen::tft.setCursor(dragArea.pos.x + 2, dragArea.pos.y + 2);
-        Screen::tft.print(name);
 
-        // Close-Button
-        closeBtn = {Vec{dragArea.pos.x + windowSize.x - closeBtnSize, dragArea.pos.y},
-                    Vec{closeBtnSize, closeBtnSize}};
-        Screen::tft.fillRect(closeBtn.pos.x, closeBtn.pos.y, closeBtn.dimensions.x, closeBtn.dimensions.y, RGB(255, 180, 180));
+        int len = (name.length() < (size.x - 20) / 6) ? name.length() : (size.x - 20) / 6;
+
+        for (int i = 0; i < len; i++)
+        {
+            Screen::tft.print(name.charAt(i));
+        }
+
+        closeBtn = {Vec{dragArea.pos.x + size.x - closeBtnSize, dragArea.pos.y}, Vec{closeBtnSize, closeBtnSize}};
+        Screen::tft.fillRect(closeBtn.pos.x, closeBtn.pos.y, closeBtn.dimensions.x, closeBtn.dimensions.y, RGB(255, 150, 150));
         Screen::tft.setCursor(closeBtn.pos.x + 4, closeBtn.pos.y + 2);
         Screen::tft.print("X");
 
-        Screen::tft.setTextSize(2); // Wieder Standardgröße
+        Screen::tft.setTextSize(2);
+    }
+
+    void drawResizeBox()
+    {
+        resizeArea = {off + size - Vec{resizeBoxSize, resizeBoxSize}, {resizeBoxSize, resizeBoxSize}};
+        Screen::tft.fillRect(resizeArea.pos.x, resizeArea.pos.y, resizeArea.dimensions.x, resizeArea.dimensions.y, RGB(180, 180, 255));
     }
 
     void drawContent()
     {
         vScreen.fillSprite(TFT_WHITE);
-        vScreen.drawString("HELLO", 10, 10, 2); // Beispielinhalt
+        vScreen.drawString("HELLO", 10, 10, 2);
         vScreen.pushSprite(off.x, off.y);
+    }
+
+    void resizeWindow(Vec delta)
+    {
+        size += delta;
+        size.x = constrain(size.x, minSize.x, maxSize.x);
+        size.y = constrain(size.y, minSize.y, maxSize.y);
+        vScreen.deleteSprite();
+        vScreen.createSprite(size.x, size.y);
     }
 
     void exit()
     {
+        // Fenster schließen
     }
 
     void loop()
     {
+        static bool wasTouchedLastFrame = false;
+
         auto pos = Screen::getTouchPos();
 
         if (pos.clicked)
         {
+            // clear the screen
             Screen::tft.fillScreen(TFT_WHITE);
 
-            // Schließen?
-            if (closeBtn.isIn(pos))
+            // Erste Berührung
+            if (!wasTouchedLastFrame)
             {
-                exit();
-                return;
+                if (closeBtn.isIn(pos))
+                {
+                    exit();
+                    return;
+                }
+                if (dragArea.isIn(pos))
+                {
+                    isDragging = true;
+                }
+                else if (resizeArea.isIn(pos))
+                {
+                    isResizing = true;
+                }
             }
 
-            // Draggen
-            if (dragArea.isIn(pos))
+            if (isDragging)
             {
                 off += pos.move;
             }
+            else if (isResizing)
+            {
+                resizeWindow(pos.move);
+            }
+
+            lastTouchPos = {pos.x, pos.y};
+            wasTouchedLastFrame = true;
+        }
+        else
+        {
+            // Kein Touch mehr
+            isDragging = false;
+            isResizing = false;
+            wasTouchedLastFrame = false;
         }
 
+        // Zeichnen
         drawTitleBar();
         drawContent();
+        drawResizeBox();
     }
 };
