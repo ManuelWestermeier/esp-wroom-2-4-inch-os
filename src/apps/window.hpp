8cccf6916e2785e60d8c9609af938feb7befdf4d
manuelwestermeier <westermeier111@gmail.com>
2025-07-24 21:24:40 +0200

    working

--- src/apps/window.hpp
+++ src/apps/window.hpp
@@ -1,66 +1,85 @@
 #pragma once
 
 #include <Arduino.h>
-
 #include "../screen/index.hpp"
 #include "../utils/rect.hpp"
 
 struct Window
 {
-    // Create sprite (virtual screen)
     TFT_eSprite vScreen = TFT_eSprite(&Screen::tft);
-    Vec off = {50, 50};
-    Rect dragArea;
-    String name = "Hello World";
 
-    void init(Vec margin = {50, 50}, Vec size = {160, 90})
+    Vec off;       // Fensterposition
+    Rect dragArea; // Bereich zum Ziehen
+    String name;   // Fenstertitel
+
+    static constexpr Vec windowSize = {160, 90};
+    static constexpr int titleBarHeight = 12;
+    static constexpr int closeBtnSize = 12;
+
+    void init(String windowName, Vec margin = {50, 50})
     {
-        vScreen.createSprite(160, 90); // Uses ~20 KB RAM
+        off = margin;
+        name = windowName;
+
+        vScreen.createSprite(windowSize.x, windowSize.y); // ~20 KB RAM
         vScreen.setTextColor(TFT_BLACK);
     }
 
-    int i = 0;
-    void loop()
+    void drawTitleBar()
     {
-        auto pos = Screen::getTouchPos();
+        // Fensterrahmen
+        Screen::tft.drawRect(
+            off.x - 1, off.y - titleBarHeight - 1,
+            windowSize.x + 2, windowSize.y + 2 + titleBarHeight,
+            TFT_BLACK);
 
-        if (pos.clicked && dragArea.isIn(pos))
-        {
-            off.x += pos.move.x;
-            off.y += pos.move.y;
-        }
+        // Drag-Area
+        dragArea = {off + Vec{0, -titleBarHeight}, {windowSize.x, titleBarHeight}};
+        Screen::tft.fillRect(
+            dragArea.pos.x, dragArea.pos.y,
+            dragArea.dimensions.x - closeBtnSize, dragArea.dimensions.y,
+            RGB(220, 220, 250));
 
-        if (pos.clicked)
-        {
-            Screen::tft.fillScreen(TFT_WHITE);
-            i = 0;
-        }
-
-        Screen::tft.drawRect(off.x - 1, off.y - 12 - 1, 160 + 2, 90 + 2 + 12, TFT_BLACK);
-
-        dragArea = {off, {160, 12}}; // 30x20 px gelbes Dragfeld oben links
-        dragArea.pos += {0, -12};
-
-        // on top left of the window
-        Screen::tft.fillRect(dragArea.pos.x, dragArea.pos.y, dragArea.dimensions.x - 12, dragArea.dimensions.y, RGB(220, 220, 250));
-
+        // Fenstertitel
         Screen::tft.setTextSize(1);
         Screen::tft.setCursor(dragArea.pos.x + 2, dragArea.pos.y + 2);
         Screen::tft.print(name);
 
-        // on top left of the window
-        Screen::tft.fillRect(dragArea.pos.x + 160 - 12, dragArea.pos.y, 12, 12, RGB(255, 180, 180));
-        Screen::tft.setCursor(dragArea.pos.x + 160 - 12 + 3, dragArea.pos.y + 2);
+        // Schließen-Button
+        Screen::tft.fillRect(
+            dragArea.pos.x + windowSize.x - closeBtnSize, dragArea.pos.y,
+            closeBtnSize, closeBtnSize,
+            RGB(255, 180, 180));
+        Screen::tft.setCursor(dragArea.pos.x + windowSize.x - closeBtnSize + 4, dragArea.pos.y + 2);
         Screen::tft.print("X");
-        Screen::tft.setTextSize(2);
 
-        // Clear virtual screen
+        Screen::tft.setTextSize(2); // Wieder Standardgröße
+    }
+
+    void drawContent()
+    {
         vScreen.fillSprite(TFT_WHITE);
-
-        // Draw something on the virtual screen
-        vScreen.drawString("HELLO", 10, 10, 2); // font size 2
-
-        // Push virtual screen to physical screen at offset
+        vScreen.drawString("HELLO", 10, 10, 2); // Beispielinhalt
         vScreen.pushSprite(off.x, off.y);
+    }
+
+    void loop()
+    {
+        auto pos = Screen::getTouchPos();
+
+        // Dragging
+        if (pos.clicked && dragArea.isIn(pos))
+        {
+            off += pos.move;
+        }
+
+        // Beispiel: Bildschirm löschen bei Klick
+        if (pos.clicked)
+        {
+            Screen::tft.fillScreen(TFT_WHITE);
+        }
+
+        drawTitleBar();
+        drawContent();
     }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ void setup()
 {
     Serial.begin(115200);
     Screen::init();
-    win.init();
+    win.init("Hello World Test");
 }
 
 void loop()
