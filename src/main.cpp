56abf6a9af3c49f9cda9413e955113e6e7e58a3c
ManuelWestermeier <westermeier111@gmail.com>
2025-07-26 15:06:33 +0200

    .

--- src/main.cpp
+++ src/main.cpp
@@ -10,29 +10,29 @@ void setup()
     Serial.begin(115200);
     delay(1000);
 
-    // Screen::init();
-    // win.init("Hello World Test");
+    Screen::init();
+    win.init("Hello World Test");
 
-    while (!SD_FS::init())
-    {
-        Serial.println("!NO SD! use an SD card (format=fat32)");
-        delay(1000);
-    }
+    // while (!SD_FS::init(5))
+    // {
+    //     Serial.println("!NO SD! use an SD card (format=fat32)");
+    //     delay(1000);
+    // }
 
-    SD_FS::writeFile("/test.txt", "Hallo von ESP32!\n");
-    String inhalt = SD_FS::readFile("/test.txt");
-    Serial.println("📖 Inhalt:\n" + inhalt);
+    // SD_FS::writeFile("/test.txt", "Hallo von ESP32!\n");
+    // String inhalt = SD_FS::readFile("/test.txt");
+    // Serial.println("📖 Inhalt:\n" + inhalt);
 
-    SD_FS::writeFile("/demo.txt", "Testinhalt\n");
-    SD_FS::getFileInfo("/demo.txt");
+    // SD_FS::writeFile("/demo.txt", "Testinhalt\n");
+    // SD_FS::getFileInfo("/demo.txt");
 
-    Serial.printf("Größe: %zu Bytes\n", SD_FS::fileSize("/demo.txt"));
-    Serial.printf("Letzte Änderung: %lu\n", SD_FS::getModifiedTime("/demo.txt"));
+    // Serial.printf("Größe: %zu Bytes\n", SD_FS::fileSize("/demo.txt"));
+    // Serial.printf("Letzte Änderung: %lu\n", SD_FS::getModifiedTime("/demo.txt"));
 
-    SD_FS::getUsageSummary();
+    // SD_FS::getUsageSummary();
 }
 
 void loop()
 {
-    // win.loop();
+    win.loop();
 }
