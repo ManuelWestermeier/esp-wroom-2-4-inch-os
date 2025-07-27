29feb1da60433673f970db4913d2d66db3099901
ManuelWestermeier <westermeier111@gmail.com>
2025-07-27 22:06:43 +0200

    working

--- src/apps/window.cpp
+++ src/apps/window.cpp
@@ -19,43 +19,57 @@ void Window::init(const String &windowName, Vec position, Vec dimensions, uint16
 {
     name = windowName;
     off = position;
-    size = {constrain(dimensions.x, minSize.x, maxSize.x), constrain(dimensions.y, minSize.y, maxSize.y)};
+    size = {
+        constrain(dimensions.x, minSize.x, maxSize.x),
+        constrain(dimensions.y, minSize.y, maxSize.y)};
 
-    bool colliding = true;
-    int movedDown = 0;
+    // Define this window's bounding rect with margin for decoration
+    auto getBoundingRect = [&](Vec pos)
+    {
+        return Rect{pos + Vec{-1, -13}, size + Vec{14, 13}}; // +14 accounts for border + margin
+    };
 
-    while (colliding && Windows::apps.size() > 0)
+    // Try to find a non-colliding vertical spot
+    const int stepY = 30;
+    const int maxTries = 50; // Prevent infinite loop
+    int tries = 0;
+
+    while (tries < maxTries)
     {
         bool collides = false;
+        Rect thisRect = getBoundingRect(off);
+
         for (auto &p : Windows::apps)
         {
             Window &w = *p;
-            // if the window collides move it down
-            if ((Rect{w.off + Vec{-1, -13}, w.size + Vec{12 + 2, 13}}.intersects(Rect{off + Vec{-1, -13}, size + Vec{12 + 2, 13}})))
+            Rect otherRect = getBoundingRect(w.off);
+            if (thisRect.intersects(otherRect))
             {
                 collides = true;
+                break;
             }
         }
-        if (collides)
-        {
-            colliding = true;
-            movedDown += 30;
-            off.y += 30;
+
+        if (!collides)
             break;
-        }
+
+        off.y += stepY;
+        tries++;
     }
 
-    for (auto &p : Windows::apps)
+    // Optional: log if the window couldn't find space
+    if (tries == maxTries)
     {
-        Window &w = *p;
-        w.off.y -= movedDown;
+        Serial.println("Warning: Could not find non-colliding position for window.");
     }
 
+    // Set icon if provided
     if (_icon != nullptr)
     {
-        memcpy(icon, _icon, sizeof(icon)); // Copies 144 * sizeof(uint16_t) = 288 Bytes
+        memcpy(icon, _icon, sizeof(icon)); // 288 bytes
     }
 
+    // Create sprites
     sprite.createSprite(size.x, size.y);
     rightSprite.createSprite(resizeBoxSize, size.y - resizeBoxSize);
     Screen::tft.fillScreen(RGB(245, 245, 255));
