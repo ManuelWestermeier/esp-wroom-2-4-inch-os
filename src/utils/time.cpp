2637cfa21c3c6f28b913510547a9884c96965c9b
ManuelWestermeier <westermeier111@gmail.com>
2025-08-05 11:41:43 +0200

    working

---
+++ src/utils/time.cpp
@@ -0,0 +1,28 @@
+#include "time.hpp"
+#include <WiFi.h>
+#include <time.h>
+
+namespace UserTime
+{
+    int isConfigured = -1; // << Nur HIER definieren
+
+    void set(int off)
+    {
+        if (isConfigured == off)
+            return;
+        if (WiFi.status() != WL_CONNECTED)
+            return;
+
+        configTime(off, off, "pool.ntp.org");
+        isConfigured = off;
+    }
+
+    tm get()
+    {
+        tm timeinfo = {};
+        timeinfo.tm_hour = 0;
+        timeinfo.tm_min = 0;
+        getLocalTime(&timeinfo);
+        return timeinfo;
+    }
+}
