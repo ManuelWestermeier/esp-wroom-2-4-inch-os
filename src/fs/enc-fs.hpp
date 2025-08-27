5037f77d16072f9091802a10fbae20fec990ba91
manuelwestermeier <westermeier111@gmail.com>
2025-08-27 23:13:49 +0200

    add enc fs wrapper

--- src/fs/enc-fs.hpp
+++ src/fs/enc-fs.hpp
@@ -1,1 +1,59 @@
 #pragma once
+
+#include <Arduino.h>
+#include <vector>
+
+#include "../auth/auth.hpp"
+
+namespace ENC_FS
+{
+    typedef std::vector<String> Path;
+    typedef std::vector<uint8_t> Buffer;
+
+    Path str2Path(String)
+    {
+    }
+    Path pathEnc(Path)
+    {
+    }
+    Path pathDec(Path)
+    {
+    }
+
+    bool exists(Path) {}
+
+    bool mkDir(Path) {}
+    bool rmDir(Path) {}
+    bool lsDir(Path) {}
+
+    Buffer readFile(Path, long start, long end)
+    {
+    }
+    String readFileString(Path)
+    {
+    }
+    bool writeFile(Path, long start, long end, Buffer data)
+    {
+    }
+    bool appendFile(Path, Buffer data)
+    {
+    }
+    bool writeFileString(Path, String data)
+    {
+    }
+    bool deleteFile(Path)
+    {
+    }
+
+    // add some metadata getters, ...
+
+    namespace Storage
+    {
+        Buffer get(String appId, long start, long end)
+        {
+        }
+        bool set(String appId, long start, long end)
+        {
+        }
+    }
+}
