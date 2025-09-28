a027a02372004c2047b70f02dad2a9eb9244d7c6
manuelwestermeier <westermeier111@gmail.com>
2025-09-28 19:38:16 +0200

    some changes

--- app/entry.lua
+++ app/entry.lua
@@ -7,7 +7,7 @@ while not WIN_closed(win) do
 
     -- Render
     WIN_drawVideo(win,
-                  "https://github.com/ManuelWestermeier/manuelwestermeier.github.io/releases/download/dev/vid-small.rgb565")
+                  "https://github.com/ManuelWestermeier/manuelwestermeier.github.io/releases/download/dev/vid-small.vam")
 
     delay(16) -- ~60 FPS
 end
