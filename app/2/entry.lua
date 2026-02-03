7e55037b4fce76889f1040c3a86168cf5d8d6917
manuelwestermeier <westermeier111@gmail.com>
2026-02-03 17:06:49 +0100

    small app change

--- app/2/entry.lua
+++ app/2/entry.lua
@@ -4,24 +4,22 @@ WIN_setName(win, "Hello World")
 while not WIN_closed(win) do
     local pressed, state, x, y, mx, my, wc, nr = WIN_getLastEvent(win, 1)
 
-    if nr then
+    if nr or pressed then
         WIN_finishFrame(win)
-    end
-
-    if nr then
+        
         WIN_fillBg(win, 1, 0xFFFF)
         WIN_writeText(win, 1, 10, 10, "Hello World", 2, 0x0000)
-    end
 
-    if pressed then
-        WIN_writeText(win, 1, 10, 30, "Clicked at: " .. x .. "," .. y, 1, 0xF800)
-    end
-
-    delay(16) -- ~60 FPS
-
+        if pressed then
+            WIN_writeText(win, 1, 10, 30, "Clicked at: " .. x .. "," .. y, 1, 0xF800)
+        end
+    else
+    
     if nr then
         WIN_writeText(win, 1, 20, 30, "R", 1, 0xF800)
     else
         WIN_writeText(win, 1, 20, 30, "N", 1, 0xF800)
     end
+
+    delay(16) -- ~60 FPS
 end
